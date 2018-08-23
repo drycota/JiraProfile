096799799574058f148104fb46b41662dfd911dd
drycota <drycota@gmail.com>
2018-08-23 14:46:21 +0300

    add ConfigProperties

--- src/main/java/Steps/LoginSteps.java
+++ src/main/java/Steps/LoginSteps.java
@@ -4,18 +4,22 @@ import Pages.LoginPageObject;
 import Pages.ProfilePageObject;
 import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Selenide.open;
+import utils.ConfigProperties;
 
 public class LoginSteps {
 
     public void LoginToJira(){
 
         //Configuration.remote = "http://localhost:4444/wd/hub";
-        Configuration.browser = "chrome";
+        //Configuration.browser = "chrome";
+        Configuration.browser = ConfigProperties.getTestProperty("useBrowser");
 
-        open("http://jira.hillel.it:8080/login.jsp");
+        //open("http://jira.hillel.it:8080/login.jsp");
+        open(ConfigProperties.getTestProperty("jiraURL"));
 
         LoginPageObject loginPage = new LoginPageObject();
-        loginPage.LoginToJira("webinar5", "webinar5");
+        loginPage.LoginToJira(ConfigProperties.getTestProperty("LoginWebinar5"),
+                              ConfigProperties.getTestProperty("PasswordWebinar5"));
         //loginPage.LoginToJira("Dmitry_Prikota", "638Jawa638!!!");
         
         ProfilePageObject openUserProfile = new ProfilePageObject();
